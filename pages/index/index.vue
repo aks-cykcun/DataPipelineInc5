6fd9593080bf5133d392cf07a1e94e7c10b536d7
aks-cykcun <milongaa066892@yeah.net>
2021-06-16 07:06:45 +0800

    1.0.1

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -30,110 +30,118 @@
 			</view>
 			<view>
 				<view class="form-item">
-						<view class="title" style="width: 180rpx;">更换商品</view>
-						<radio-group name="radio"  @change="goodsChange">
-								<view class="radio">
-										<radio value="001" checked/><text>商品1：多组多规格商品</text>
-								</view>
-								<view class="radio">
-										<radio value="002" /><text>商品2：单组多规格商品</text>
-								</view class="radio">
-								<view class="radio">
-										<radio value="003" /><text>商品3：单组单规格商品</text>
-								</view>
-								<view class="radio">
-										<radio value="004" /><text>商品4：暂无库存商品</text>
-								</view>
-						</radio-group>
+					<view class="title" style="width: 180rpx;">更换商品</view>
+					<radio-group name="radio"  @change="goodsChange">
+						<view class="radio">
+							<radio value="001" checked/><text>商品1：多组多规格商品</text>
+						</view>
+						<view class="radio">
+							<radio value="002" /><text>商品2：单组多规格商品</text>
+						</view class="radio">
+						<view class="radio">
+							<radio value="003" /><text>商品3：单组单规格商品</text>
+						</view>
+						<view class="radio">
+							<radio value="004" /><text>商品4：暂无库存商品</text>
+						</view>
+					</radio-group>
 				</view>
 				
 				<view class="form-item">
-						<view class="title" style="width: 180rpx;">模式</view>
-						<radio-group name="radio"  @change="skuModeChange">
-								<view class="radio">
-										<radio value="1" checked/><text>都显示</text>
-								</view>
-								<view class="radio">
-										<radio value="2" /><text>只显示购物车</text>
-								</view class="radio">
-								<view class="radio">
-										<radio value="3" /><text>只显示立即购买</text>
-								</view>
-						</radio-group>
-				</view>
-				<view class="form-item">
-						<view class="title">立即购买文字</view>
-						<view class="input-view">
-								<input class="input" v-model="form.buyNowText"/>
+					<view class="title" style="width: 180rpx;">模式</view>
+					<radio-group name="radio"  @change="skuModeChange">
+						<view class="radio">
+							<radio value="1" checked/><text>都显示</text>
 						</view>
-				</view>
-				<view class="form-item">
-						<view class="title">立即购买文字颜色</view>
-						<view class="input-view">
-								<input class="input" v-model="form.buyNowColor"/>
+						<view class="radio">
+							<radio value="2" /><text>只显示购物车</text>
+						</view class="radio">
+						<view class="radio">
+							<radio value="3" /><text>只显示立即购买</text>
 						</view>
+					</radio-group>
 				</view>
 				<view class="form-item">
-						<view class="title">立即购买按钮背景色</view>
-						<view class="input-view">
-								<input class="input" v-model="form.buyNowBackgroundColor"/>
-						</view>
+					<view class="title">立即购买文字</view>
+					<view class="input-view">
+						<view style="width: 30rpx;height: 30rpx;"></view>
+						<input class="input" v-model="form.buyNowText"/>
+					</view>
 				</view>
 				<view class="form-item">
-						<view class="title">加入购物车文字</view>
-						<view class="input-view">
-								<input class="input" v-model="form.addCartText"/>
-						</view>
+					<view class="title">立即购买文字颜色</view>
+					<view class="input-view">
+						<view :style="'background-color: '+form.buyNowColor+';width: 30rpx;height: 30rpx;'"></view>
+						<input class="input" v-model="form.buyNowColor"/>
+					</view>
 				</view>
 				<view class="form-item">
-						<view class="title">加入购物车文字颜色</view>
-						<view class="input-view">
-								<<input class="input" v-model="form.addCartColor"/>
-						</view>
+					<view class="title">立即购买按钮背景色</view>
+					<view class="input-view">
+						<view :style="'background-color: '+form.buyNowBackgroundColor+';width: 30rpx;height: 30rpx;'"></view>
+						<input class="input" v-model="form.buyNowBackgroundColor"/>
+					</view>
 				</view>
 				<view class="form-item">
-						<view class="title">加入购物车按钮背景色</view>
-						<view class="input-view">
-								<input class="input" v-model="form.addCartBackgroundColor"/>
-						</view>
+					<view class="title">加入购物车文字</view>
+					<view class="input-view">
+						<view style="width: 30rpx;height: 30rpx;"></view>
+						<input class="input" v-model="form.addCartText"/>
+					</view>
 				</view>
 				<view class="form-item">
-						<view class="title">无库存时按钮文字</view>
-						<view class="input-view">
-								<input class="input" v-model="form.noStockText"/>
-						</view>
+					<view class="title">加入购物车文字颜色</view>
+					<view class="input-view">
+						<view :style="'background-color: '+form.addCartColor+';width: 30rpx;height: 30rpx;'"></view>
+						<input class="input" v-model="form.addCartColor"/>
+					</view>
 				</view>
 				<view class="form-item">
-						<view class="title">价格的字体颜色</view>
-						<view class="input-view">
-								<input class="input" v-model="form.priceColor"/>
-						</view>
+					<view class="title">加入购物车按钮背景色</view>
+					<view class="input-view">
+						<view :style="'background-color: '+form.addCartBackgroundColor+';width: 30rpx;height: 30rpx;'"></view>
+						<input class="input" v-model="form.addCartBackgroundColor"/>
+					</view>
 				</view>
 				<view class="form-item">
-						<view class="title">最小购买数量</view>
-						<vk-u-number-box
-							v-model="form.minBuyNum" :min="1" :max="10000" :step="1" :positive-integer="true">
-						</vk-u-number-box>
+					<view class="title">无库存时按钮文字</view>
+					<view class="input-view">
+						<view style="width: 30rpx;height: 30rpx;"></view>
+						<input class="input" v-model="form.noStockText"/>
+					</view>
 				</view>
 				<view class="form-item">
-						<view class="title">最大购买数量</view>
-						<vk-u-number-box
-							v-model="form.maxBuyNum" :min="1" :max="10000" :step="1" :positive-integer="true">
-						</vk-u-number-box>
+					<view class="title">价格的字体颜色</view>
+					<view class="input-view">
+						<view :style="'background-color: '+form.priceColor+';width: 30rpx;height: 30rpx;'"></view>
+						<input class="input" v-model="form.priceColor"/>
+					</view>
 				</view>
 				<view class="form-item">
-						<view class="title">步进器步长</view>
-						<vk-u-number-box
-							v-model="form.stepBuyNum" :min="1" :max="10000" :step="1" :positive-integer="true">
-						</vk-u-number-box>
+					<view class="title">最小购买数量</view>
+					<vk-u-number-box
+						v-model="form.minBuyNum" :min="1" :max="10000" :step="1" :positive-integer="true">
+					</vk-u-number-box>
 				</view>
 				<view class="form-item">
-						<view class="title">显示关闭按钮</view>
-						<switch checked @change="showCloseChange" />
+					<view class="title">最大购买数量</view>
+					<vk-u-number-box
+						v-model="form.maxBuyNum" :min="1" :max="10000" :step="1" :positive-integer="true">
+					</vk-u-number-box>
 				</view>
 				<view class="form-item">
-						<view class="title">点击遮罩关闭组件</view>
-						<switch checked @change="maskCloseAbleChange" />
+					<view class="title">步进器步长</view>
+					<vk-u-number-box
+						v-model="form.stepBuyNum" :min="1" :max="10000" :step="1" :positive-integer="true">
+					</vk-u-number-box>
+				</view>
+				<view class="form-item">
+					<view class="title">显示关闭按钮</view>
+					<switch checked @change="showCloseChange" />
+				</view>
+				<view class="form-item">
+					<view class="title">点击遮罩关闭组件</view>
+					<switch checked @change="maskCloseAbleChange" />
 				</view>
 		</view>
 		</view>
@@ -284,6 +292,7 @@
 <style lang="scss" scoped>
 	.app {
 		padding: 30rpx;
+		font-size: 28rpx;
 	}
 	.form-item{
 		display: flex;
@@ -301,6 +310,7 @@
 		border: 1px solid #d6d6d6;
 		border-radius: 10rpx;
 		padding: 8rpx 30rpx;
+		font-size: 28rpx;
 	}
 	.radio{
 		padding: 6rpx 0rpx;

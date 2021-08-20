2c750738736817e83df6dbbbbd3e2ba54f560d87
aks-cykcun <milongaa066892@yeah.net>
2021-08-20 21:30:56 +0800

    更新静态页面示例

--- pages/index/index-static.vue
+++ pages/index/index-static.vue
@@ -51,51 +51,89 @@
         // 此处写接口请求，并将返回的数据进行处理成goodsInfo的数据格式，
         // goodsInfo是后端返回的数据
         that.goodsInfo = {
-          "_id":"002",
-          "name": "迪奥香水",
-          "goods_thumb":"https://res.lancome.com.cn/resources/2020/9/11/15998112890781924_920X920.jpg?version=20200917220352530",
-          "sku_list": [
-            {
-              "_id": "004",
-              "goods_id": "002",
-              "goods_name": "迪奥香水",
-              "image": "https://res.lancome.com.cn/resources/2020/9/11/15998112890781924_920X920.jpg?version=20200917220352530",
-              "price": 19800,
-              "sku_name_arr": ["50ml/瓶"],
-              "stock": 100
-            },
-            {
-              "_id": "005",
-              "goods_id": "002",
-              "goods_name": "迪奥香水",
-              "image": "https://res.lancome.com.cn/resources/2020/9/11/15998112890781924_920X920.jpg?version=20200917220352530",
-              "price": 9800,
-              "sku_name_arr": ["70ml/瓶"],
-              "stock": 100
-            }
-          ],
-          "spec_list": [
-            {
-              "list": [
-                {
-                  "name": "20ml/瓶"
-                },
-                {
-                  "name": "50ml/瓶"
-                },
-                {
-                  "name": "70ml/瓶"
-                }
-              ],
-              "name": "规格"
-            }
-          ]
-        }
+					"_id":"001",
+					"name": "iphone11",
+					"goods_thumb":"https://img14.360buyimg.com/n0/jfs/t1/59022/28/10293/141808/5d78088fEf6e7862d/68836f52ffaaad96.jpg",
+					"sku_list": [
+						{
+							"_id": "001",
+							"goods_id": "001",
+							"goods_name": "iphone11",
+							"image": "https://img14.360buyimg.com/n0/jfs/t1/79668/22/9987/159271/5d780915Ebf9bf3f4/6a1b2703a9ed8737.jpg",
+							"price": 19800,
+							"sku_name_arr": ["红色", "128G", "公开版"],
+							"stock": 100
+						},
+						{
+							"_id": "002",
+							"goods_id": "001",
+							"goods_name": "iphone11",
+							"image": "https://img14.360buyimg.com/n0/jfs/t1/52252/35/10516/124064/5d7808e0E46202391/7100f3733a1c1f00.jpg",
+							"price": 9800,
+							"sku_name_arr": ["白色", "256G","公开版"],
+							"stock": 100
+						},
+						{
+							"_id": "003",
+							"goods_id": "001",
+							"goods_name": "iphone11",
+							"image": "https://img14.360buyimg.com/n0/jfs/t1/79668/22/9987/159271/5d780915Ebf9bf3f4/6a1b2703a9ed8737.jpg",
+							"price": 19800,
+							"sku_name_arr": ["红色","256G","公开版"],
+							"stock": 100
+						}
+					],
+					"spec_list": [
+						{
+							"list": [
+								{
+									"name": "红色"
+								},
+								{
+									"name": "黑色"
+								},
+								{
+									"name": "白色"
+								}
+							],
+							"name": "颜色"
+						},
+						{
+							"list": [
+								{
+									"name": "128G"
+								},
+								{
+									"name": "256G"
+								}
+							],
+							"name": "内存"
+						},
+						{
+							"list": [
+								{
+									"name": "公开版"
+								},
+								{
+									"name": "非公开版"
+								}
+							],
+							"name": "版本"
+						}
+					]
+				};
         that.skuKey = true;
       },
       // sku组件 开始-----------------------------------------------------------
       onOpenSkuPopup(){
         console.log("监听 - 打开sku组件");
+				
+				// 下面的代码作用是当弹窗打开时，自动选中 SKU 红色/128G/公开版，数量为1件
+				
+				// that.$refs.skuPopup.selectSku({
+				// 	sku:["红色","128G","公开版"],
+				// 	num:1
+				// });
       },
       onCloseSkuPopup(){
         console.log("监听 - 关闭sku组件");
